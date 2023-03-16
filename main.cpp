e92b9e7a62b7a5b52683f89223ebb1dab62060e1
FacundoBaez1994 <fbaez@fi.uba.ar>
2023-03-16 23:58:55 +0000

    se agrego mensaje de estado del led

--- main.cpp
+++ main.cpp
@@ -9,15 +9,22 @@ int main()
 
     gasDetector.mode(PullDown);
 
+    bool ledStatus = OFF;
+
     alarmLed = OFF;
+
     printf ("hola!\n"); // baudrate 9600
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
+            ledStatus = ON;
         }
-            printf ("while\n"); // baudrate 9600
+
+        printf ("%d\n", ledStatus); // baudrate 9600
+
         if ( gasDetector == OFF ) {
             alarmLed = OFF;
+            ledStatus = OFF;
         }
     }
 }
