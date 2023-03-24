4a7e04c93c258507df65b88ca4621fc5d25b7756
FacundoBaez1994 <fbaez@fi.uba.ar>
2023-03-24 00:51:13 +0000

    Se paso el codigo a C

--- main.cpp
+++ main.cpp
@@ -5,25 +5,37 @@ int main()
 {
     DigitalIn gasDetector(D2);
 
-    DigitalOut alarmLed(LED1);
+    gpio_t alarmLedC; // En C variable tipo gpio_t general port input output
+    gpio_init_out (&alarmLedC, LED1); // En C defino como salida al pin LED1
 
-    gasDetector.mode(PullDown);
+     gpio_t gasDetectorC; // En C variable tipo gpio_t general port input output
+    gpio_init_in (&gasDetectorC , D2); // En C defino como entrada al pin D2
+    // DigitalOut alarmLed(LED1); C++
+
+    core_util_critical_section_enter(); // En C
+    gpio_mode(&gasDetectorC, PullDown);
+    core_util_critical_section_exit();
+
+   //  gasDetector.mode(PullDown); En C++
 
     bool ledStatus = OFF;
 
-    alarmLed = OFF;
+   // alarmLed = OFF;  En C++
+    gpio_write(&alarmLedC, OFF);
 
     printf ("hola!\n"); // @note usar baudrate 9600
     while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
+        if ( gpio_read (&gasDetectorC) == ON ) {
+               gpio_write(&alarmLedC, ON);
   
         }
 
-        printf ("%i\n", alarmLed.read()); // @note imprime el estado del led
+        // printf ("%i\n", alarmLed.read()); // @note imprime el estado del led
+        
+        printf ("LED %i\n", gpio_read(&alarmLedC));  //C
 
-        if ( gasDetector == OFF ) {
-            alarmLed = OFF;
+        if ( gpio_read (&gasDetectorC) == OFF ) {
+            gpio_write(&alarmLedC, OFF);
        
         }
     }
